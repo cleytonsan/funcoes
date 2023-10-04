4f65cab5acb0d1f27796ebdad245a7b4b1f82f8e
cleyton santos da silva <58754770+cleytonsan@users.noreply.github.com>
2023-10-04 10:57:40 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,46 @@
+/*
+Função: trecho de codigo que só é executado quando é chamado/invocado
+    
+    Função void(vazia)
+    Função com parametros(s)
+    Função return
+    Função arrow/ arrow function
+*/
+
+// let valor = 20
+// function incrementa(){
+//     valor = 20 + 30
+// }
+// incrementa()
+// console.log(valor)
+
+
+// function mostraNome(nome){
+//    console.log(nome)
+// }
+// mostraNome('cleyton')
+// mostraNome('Salve Salve')
+
+
+// function soma(n1, n2){
+//     const somaDosNumeros = n1 + n2
+//     console.log(somaDosNumeros)
+//  }
+//  soma(23, 45)
+//  soma(500, 665)
+
+
+// function soma(n1, n2){
+//     const somaDosNumeros = n1 + n2
+//     return somaDosNumeros
+// }
+// const resultado = soma(5, 6)
+// console.log(resultado)
+
+
+const multi = (n1, n2) => {
+    const multiplicacao = n1 * n2
+    return multiplicacao
+}
+const res = multi(6, 8)
+console.log(res)
